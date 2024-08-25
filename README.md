b206d82e3932f6ff7a3fd0e342048366357d398a
rileyeh <rhatchdev@gmail.com>
2024-08-25 15:03:52 -0700

    update for use with approx

--- README.md
+++ README.md
@@ -10,18 +10,13 @@ Actually there's no way to allow ChatGPT access a GitHub repository, inclusing t
 
 We will obtain this integration thanks to GitHub actions. This repository contains an Action that performs the following tasks.
 
-* Pull Request checking and analizing, providing comments abou the repo.
+- Pull Request checking and analizing, providing comments abou the repo.
 
 ## Use this action in your CI/CD
 
 You need to create two secrets in order to run this action
 
-* `OPENAI_API_KEY` Create an OpenAI developer account and obtain an API acces key
-* `GH_TOKEN` Create a GitHub developer access token
-
-Other fields that you can customize are
-
-* `dev-lang` Define the programming language using in your repository
+- `OPENAI_API_KEY` Create an OpenAI developer account and obtain an API acces key
 
 ```yaml
 name: App Code Review
@@ -43,11 +38,10 @@ jobs:
           openai-api-key: ${{ secrets.OPENAI_API_KEY }}
           github-token: ${{ secrets.GH_TOKEN }}
           github-pr-id: ${{ github.event.number }}
-          dev-lang: Swift
           openai-max-tokens: 4096
 ```
 
-## GitHub Action 
+## GitHub Action
 
 Our shell script must be executable. Make sure the `main.sh` file has execute permissions before using it in a workflow. You can modify the permission from your terminal using these commands.
 
@@ -60,5 +54,5 @@ $ git update-index --chmod=+x entrypoint.sh
 
 ## Contact
 
-* **Email** [adolfo.vera@globant.com](mailto:adolfo.vera@globant.com)
-* **LinkedIn** [https://www.linkedin.com/in/adolfo-vera](https://www.linkedin.com/in/adolfo-vera)
+- **Email** [adolfo.vera@globant.com](mailto:adolfo.vera@globant.com)
+- **LinkedIn** [https://www.linkedin.com/in/adolfo-vera](https://www.linkedin.com/in/adolfo-vera)
--- main.py
+++ main.py
@@ -11,9 +11,6 @@ def code_review(parameters: dict):
     repo = github_client.get_repo(os.getenv('GITHUB_REPOSITORY'))
     pull_request = repo.get_pull(parameters["pr_id"])
 
-    resume = make_resume_for_pull_request(pr=pull_request)
-    pull_request.create_issue_comment(resume)
-
     commits = pull_request.get_commits()
 
     for commit in commits:
@@ -35,32 +32,16 @@ def code_review(parameters: dict):
                     temperature=parameters['temperature']
                 )
 
-                pull_request.create_issue_comment(f"ChatGPT's review about `{file.filename}` file:\n {response['choices'][0]['message']['content']}")
+                pull_request.create_issue_comment(f"Review for `{file.filename}` file:\n {response['choices'][0]['message']['content']}")
             except Exception as ex:
                 message = f"🚨 Fail code review process for file **{filename}**.\n\n`{str(ex)}`"
                 pull_request.create_issue_comment(message)
 
 
-def make_prompt(dev_lang: str) -> str:
-    review_prompt = f"Review this {dev_lang} code for potential bugs or Code Smells and suggest improvements. Generate your response in markdown format"
+def make_prompt() -> str:
+    review_prompt = f"You are a manager of a new hire who is submitting code for a project you're working on. You will be reviewing the new hire's code and giving feedback on how well the code aligns with the goal of the project as well as suggestions about their code and how to improve. You should give feedback on how well their code follows industry best practices. You should not give them the answers to any problems, but should point out where there is room for improvement and perhaps give advice on where to look, but do not give them the solution. Generate your response in markdown format"
 
     return review_prompt
-
-
-def make_resume_for_pull_request(pr: PullRequest) -> str:
-    comment = f"""
-    Starting review process for this pull request send by **{pr.user.name}**
-    **Commits** in this pull request: {pr.commits}
-
-    **Additions**: {pr.additions}
-    **Changed** files: {pr.changed_files}
-    **Deletions**: {pr.deletions}
-    """
-
-    comment = comment.format(length='multi-line', ordinal='second')
-
-    return comment
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -68,8 +49,7 @@ if __name__ == "__main__":
     parser.add_argument('--openai-api-key', help='Your OpenAI API Key')
     parser.add_argument('--github-token', help='Your Github Token')
     parser.add_argument('--github-pr-id', help='Your Github PR ID')
-    parser.add_argument('--dev-lang', help='Development language used for this request')
-    parser.add_argument('--openai-engine', default="gpt-3.5-turbo", help='GPT-3.5 model to use. Options: text-davinci-003, text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
+    parser.add_argument('--openai-engine', default="gpt-4o", help='GPT-4o model to use. Options: text-davinci-003, text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
     parser.add_argument('--openai-temperature', default=0.0, help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
     parser.add_argument('--openai-max-tokens', default=4096, help='The maximum number of tokens to generate in the completion.')
     
@@ -80,7 +60,7 @@ if __name__ == "__main__":
 
     review_parameters = {
         "pr_id" : int(args.github_pr_id),
-        "prompt" : make_prompt(dev_lang=args.dev_lang),
+        "prompt" : make_prompt(),
         "temperature" : float(args.openai_temperature),
         "max_tokens" : int(args.openai_max_tokens),
         "model" : args.openai_engine
